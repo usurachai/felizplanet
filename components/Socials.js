89233dd82dbee10f2243a82c1230996a88bdbfdd
usurachai <toroaek@hotmail.com>
2022-03-15 15:46:27 +0700

    fix the footer discord url again

--- components/Socials.js
+++ components/Socials.js
@@ -8,7 +8,7 @@ export default function Socials({ className }) {
         <div className={styles.Socials + " " + className}>
             <ImageLink
                 src="/images/social/discord.png"
-                href=" https://discord.gg/qvN3ZC6DvB"
+                href="https://discord.gg/qvN3ZC6DvB"
                 className={styles.socialIcon1}
             />
             <ImageLink
